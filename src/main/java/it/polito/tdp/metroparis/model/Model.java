7efb6fe05fb92d61944566ed0230870f4a790de6
Borghi <Borghi@HeroBook.lan>
2021-05-05 12:24:48 +0200

    Soluzione

--- src/main/java/it/polito/tdp/metroparis/model/Model.java
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -1,17 +1,28 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
 
 import it.polito.tdp.metroparis.db.MetroDAO;
 
 public class Model {
 	
 	Graph<Fermata, DefaultEdge> grafo ;
+	
+	Map<Fermata, Fermata> predecessore;
 
 	public void creaGrafo() {
 		this.grafo = new SimpleGraph<>(DefaultEdge.class) ;
@@ -44,5 +55,87 @@ public class Model {
 				this.grafo.vertexSet().size(), this.grafo.edgeSet().size()) ;
 //		System.out.println(this.grafo) ;
 	}
+	
+	public List<Fermata> fermateRaggiungibili(Fermata partenza){
+//      visita in profondita 		
+//		DepthFirstIterator<Fermata, DefaultEdge> dfv = 
+//		new DepthFirstIterator<>(this.grafo, partenza) ;
+		
+//      visita in ampiezza (utile per i cammini minimi)
+		BreadthFirstIterator<Fermata,DefaultEdge> bfv =  new BreadthFirstIterator<>(this.grafo, partenza);
+		
+		this.predecessore = new HashMap<>();
+		this.predecessore.put(partenza, null);
+		
+		bfv.addTraversalListener(new TraversalListener<Fermata, DefaultEdge>(){
+
+			@Override
+			public void connectedComponentFinished(ConnectedComponentTraversalEvent e) {
+			}
+
+			@Override
+			public void connectedComponentStarted(ConnectedComponentTraversalEvent e) {
+		    }
+
+			@Override
+			public void edgeTraversed(EdgeTraversalEvent<DefaultEdge> e) {
+				DefaultEdge arco = e.getEdge();
+				Fermata a = grafo.getEdgeSource(arco);
+				Fermata b = grafo.getEdgeTarget(arco);
+				
+				if(predecessore.containsKey(b) && !predecessore.containsKey(a)) {
+					predecessore.put(a, b);
+				//	System.out.println(a + "scoperto da "+ b);
+				} else if(predecessore.containsKey(a) && !predecessore.containsKey(b)){
+					predecessore.put(b, a);
+				//	System.out.println(b + "scoperto da "+ a);
+				}
+			}
+
+			@Override
+			public void vertexTraversed(VertexTraversalEvent<Fermata> e) {
+//				System.out.println(e.getVertex());
+			}
+
+			@Override
+			public void vertexFinished(VertexTraversalEvent<Fermata> e) {
+			}
+			
+		});
+		
+		List<Fermata> result = new ArrayList<>();
+		
+		while(bfv.hasNext()) {
+			Fermata f = bfv.next();
+			result.add(f);
+		}
+		return result;
+	}
+	
+	public Fermata trovaFermata(String nome) {
+		for (Fermata f : this.grafo.vertexSet()) {
+			if (f.getNome().equals(nome)) {
+				return f;
+			}
+		}
+		return null;
+	}
+	
+	public List<Fermata> trovaCammino(Fermata partenza, Fermata arrivo){
+		fermateRaggiungibili(partenza);
+		
+		List<Fermata> result = new LinkedList<>();
+		result.add(arrivo);
+		Fermata f = arrivo;
+		while (predecessore.get(f)!=null) {
+			f = predecessore.get(f);
+			result.add(0, f);
+		}
+		return result;
+	}
+	
+	
+	
+	
 	
 }
